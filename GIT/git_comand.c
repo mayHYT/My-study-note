e4c22fd16c2e309ef51549c2273e59fc27ef935c
may <13263440344@163.com>
2018-09-13 17:22:38 +0800

    提交git_command.c文件

---
+++ GIT/git_comand.c
@@ -0,0 +1,35 @@
+
+
+﻿时间=2016/5/20	收盘点位	        市值	流通市值	PE-TTM(加权平均值)	PE-TTM分位点(加权平均值)	PE-TTM危险值(加权平均值)	PE-TTM中位值(加权平均值)	PE-TTM机会值(加权平均值)
+
+5/20/2016	3078.22	2.5225E+13	1.87478E+13	11.6494	0.3351	25.979	14.609	10.3906
+
+﻿时间	收盘点位	市值	流通市值	PB(加权平均值)	PB分位点(加权平均值)	PB危险值(加权平均值)	PB中位值(加权平均值)	PB机会值(加权平均值)
+
+5/20/2016	3078.22	2.5225E+13	1.87478E+13	1.3703	0.1138	3.2289	2.1852	1.4897
+
+
+
+5/20/2016	5732.9	6.68192E+12	5.2122E+12	44.3806	0.6348	64.3459	38.6899	28.5676
+
+5/20/2016	5732.9	6.68192E+12	5.2122E+12	2.753	0.4039	3.995	3.0257	2.3136
+
+
+/project/swbuild/ehuamay/trees/sqa/ARTS/logs/ft1729348_pm_improvement_netconf_part2_091218_034326.log
+
+/project/swbuild/ehuamay/trees/sqa/ARTS/logs/ft1729348_pm_improvement_netconf_part1_091218_081150.log
+
+
+git remote add origin https://github.com/mayHYT/My-study-note.git
+
+ git remote -v
+
+ git push origin master
+
+ git pull origin master
+ 
+  git clone https://github.com/MayHYT/My-study-note
+  
+  《你的灯亮着吗？》
+  《探索需求》
+  《麦肯锡教我的思考武器》
